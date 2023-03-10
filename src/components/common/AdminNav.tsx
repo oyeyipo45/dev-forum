fe37e1f238a7027ac0051a6a89c0aa13b8b30d25
damilola oyeyipo <oyeyipo45@gmail.com>
2023-03-10 11:34:28 +0100

    Rearrangge and  make nav bar reusable

--- src/components/common/AdminNav.tsx
+++ src/components/common/AdminNav.tsx
@@ -1,45 +1,33 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
-import Logo from '../Images/Logo';
-import { AiOutlineDashboard, AiOutlineContainer, AiOutlineTeam, AiOutlineMail } from 'react-icons/ai';
+import { IconType } from 'react-icons';
+import Logo from '../images/Logo';
 
-interface Props {}
+interface Props {
+  navItems: { label: string; icon: IconType; href: string }[];
+}
 
-const AdminNav: NextPage<Props> = (): JSX.Element => {
+const AdminNav: NextPage<Props> = ({ navItems }): JSX.Element => {
   return (
     <nav className='h-screen w-60 shadow-sm bg-secondary-light dark:bg-secondary-dark'>
-      <Link href='/admin' passHref legacyBehavior>
-        <a className='flex items-center space-x-2 p-3'>
+      <Link href='/admin' className='flex items-center space-x-2 p-3'>
           <Logo className='fill-highlight-light dark:fill-highlight-dark   w-5 h-5' />
           <span className='fill-highlight-light dark:fill-highlight-dark text-xl font-semibold'>Admin</span>
-        </a>
       </Link>
 
       <div className='space-y-4 mt-6'>
-        <Link href='/admin' passHref legacyBehavior>
-          <a className='flex items-center p-3 mb-1 hover:scale-[0.98] transition fill-highlight-light dark:fill-highlight-dark text-xl '>
-            <AiOutlineDashboard size={24} />
-            <span className='ml-4'>Dashboard</span>{' '}
-          </a>
-        </Link>
-        <Link href='/admin/posts' passHref legacyBehavior>
-          <a className='flex items-center p-3 mb-1 hover:scale-[0.98] transition fill-highlight-light dark:fill-highlight-dark text-xl '>
-            <AiOutlineContainer size={24} />
-            <span className='ml-4'>Posts</span>{' '}
-          </a>
-        </Link>
-        <Link href='/admin/users' passHref legacyBehavior>
-          <a className='flex items-center p-3 mb-1 hover:scale-[0.98] transition fill-highlight-light dark:fill-highlight-dark text-xl '>
-            <AiOutlineTeam size={24} />
-            <span className='ml-4'>Users</span>{' '}
-          </a>
-        </Link>
-        <Link href='/admin/comments' passHref legacyBehavior>
-          <a className='flex items-center p-3 mb-1 hover:scale-[0.98] transition fill-highlight-light dark:fill-highlight-dark text-xl '>
-            <AiOutlineMail size={24} />
-            <span className='ml-4'>Comments</span>{' '}
-          </a>
-        </Link>
+        {navItems.map((item) => {
+          return (
+            <Link
+              href={item.href}
+              key={item.label}
+              className='flex items-center p-3 mb-1 hover:scale-[0.98] transition fill-highlight-light dark:fill-highlight-dark text-xl '
+            >
+              <item.icon size={24} />
+              <span className='ml-4'>{item.label}</span>{' '}
+            </Link>
+          );
+        })}
       </div>
     </nav>
   );
--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,10 +1,44 @@
 import AdminNav from '@/components/common/AdminNav';
+import { NextPage } from 'next';
 import { FC } from 'react';
+import { AiOutlineContacts, AiOutlineContainer, AiOutlineDashboard, AiOutlineMail, AiOutlineTeam } from 'react-icons/ai';
 
 interface Props {}
 
-const Admin: FC<Props> = (props): JSX.Element => {
-  return <div><AdminNav /></div>;
+const navItems = [
+  {
+    href: '/admin',
+    icon: AiOutlineDashboard,
+    label: 'Dashboard',
+  },
+  {
+    href: '/admin/posts',
+    icon: AiOutlineContainer,
+    label: 'Posts',
+  },
+  {
+    href: '/admin/users',
+    icon: AiOutlineTeam,
+    label: 'Users',
+  },
+  {
+    href: '/admin/comments',
+    icon: AiOutlineMail,
+    label: 'Comments',
+  },
+  {
+    href: '/admin/contacts',
+    icon: AiOutlineContacts,
+    label: 'Contact',
+  },
+];
+
+const Admin: NextPage<Props> = (): JSX.Element => {
+  return (
+    <div>
+      <AdminNav navItems={navItems} />
+    </div>
+  );
 };
 
 export default Admin;
